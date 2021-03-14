9686c7dcf1c0cd2ccfdbd153528baf64d2bc9e9b
Derek-94 <ghdtjrrl94@gmail.com>
2021-03-14 18:52:10 +0900

    feat(component): Update TodoList items rendering component.
    - Used react hooks to give props from parent to child.
    - Used classnames npm lib.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import TodoTemplate from "./components/TodoTemplate"
 import TodoInsert from "./components/TodoInsert"
 import TodoList from "./components/TodoList"
 
 const App = () => {
+  const [todos, setTodo] = useState([
+    {
+      id: 1,
+      text: "example1",
+      checked: true
+    },
+    {
+      id: 2,
+      text: "example2",
+      checked: true
+    },
+    {
+      id: 3,
+      text: "example3",
+      checked: true
+    }
+  ])
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos = {todos} />
     </TodoTemplate>
   )
 }
